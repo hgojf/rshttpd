5773cb26b1ca7cbd30012dbca46794736be4002c
Henry Ford <fordhenry2299@gmail.com>
2024-01-26 22:04:41 +0000

    Add a test case for MimeDb parsing

--- src/mime.rs
+++ src/mime.rs
@@ -2,7 +2,7 @@ use tokio::io::AsyncBufReadExt;
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct MimeDb(HashMap<String, String>);
 
 #[derive(Debug)]
@@ -31,5 +31,22 @@ impl MimeDb {
 	pub fn get(&self, file: &str) -> Option<&str> {
 		let (_, file) = file.rsplit_once('.')?;
 		self.0.get(file).map(|x| x.as_str())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[tokio::test]
+	async fn parse() {
+		let db = include_bytes!("../tests/mime.types");
+		let mut db = tokio::io::BufReader::new(&db[..]);
+		let db = MimeDb::new(&mut db).await.unwrap();
+		let wanted = [ 
+		("atom", "application/atom+xml"), ("woff", "application/font-woff"),
+		("jar", "application/java-archive"), ("war", "application/java-archive")
+		].iter().map(|(a, b)| (a.to_string(), b.to_string()));
+		let wanted = MimeDb(HashMap::from_iter(wanted));
+		assert_eq!(wanted, db);
 	}
 }
